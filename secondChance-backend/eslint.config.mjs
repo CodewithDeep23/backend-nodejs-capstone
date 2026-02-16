37ff8b49973b741cb5f977569e794876ba9f0be3
CodewithDeep23 <deepankarsinghvm@gmail.com>
2026-02-16 11:11:54 +0530

    update

--- secondChance-backend/eslint.config.mjs
+++ secondChance-backend/eslint.config.mjs
@@ -1,7 +1,31 @@
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
 
-export default defineConfig([
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: globals.node
+    },
+    rules: {
+      semi: ['warn', 'never'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'comma-dangle': ['error', 'never'],
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'space-before-function-paren': ['error', 'always'],
+      'keyword-spacing': ['error', { before: true, after: true }],
+      'space-infix-ops': 'error',
+      'eol-last': ['error', 'always'],
+      'no-unused-vars': ['warn'],
+      'no-trailing-spaces': 'error',
+      'space-in-parens': ['error', 'never'],
+      'block-spacing': ['error', 'always'],
+      'no-undef': ['warn']
+    }
+  }
+]
